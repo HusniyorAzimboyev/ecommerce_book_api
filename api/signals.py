293477cbdd60127c18d0a0ad541bb8258473a2ef
Husniyor <husniyor09@gmail.com>
2025-01-06 19:52:31 +0500

    added replanish stock,models. Added signals: admin will be notified

---
+++ api/signals.py
@@ -0,0 +1,16 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+import requests
+from api.models import Order
+from config.settings import TELEGRAM_BOT_TOKEN,ADMIN_ID
+
+@receiver(post_save,sender=Order)
+def notify_admin(sender,instance,created,**kwargs):
+    if created:
+        token = TELEGRAM_BOT_TOKEN
+        method = "send_Message"
+        message_text = f"New Order: {instance.id}\nBook: {instance.product.title}\nQuantity: {instance.quantity}\nClient: {instance.customer.username}\nTel: {instance.phone_number}"
+        response = requests.post(url=f"https://api.telegram.org/bot{token}/{method}",
+                      data={
+                          'chat_id':ADMIN_ID,"text":message_text
+                      }).json()
