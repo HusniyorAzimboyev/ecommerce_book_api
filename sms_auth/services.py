095500cf6d6455ca7d9019b771f3789626bc6274
Husniyor <husniyor09@gmail.com>
2025-02-04 17:17:54 +0500

    edited sms-auth

--- sms_auth/services.py
+++ sms_auth/services.py
@@ -23,7 +23,7 @@ def send_sms_via_infobip(serializer):
     "messages": [
         {
             "destinations": [{"to":phone_number}],
-            "from": "Book_ecommerce_Hugo",
+            "from": "HBooks.uz",
             "text": f"your verification code is {verification_code}"
         }
     ]
